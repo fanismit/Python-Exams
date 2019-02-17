c2d4d5258824f0556994c0558c86b4f628f21f60
fanismit <47718787+fanismit@users.noreply.github.com>
2019-02-17 22:34:57 +0200

    Update Pythonex4.py

--- Pythonex4.py
+++ Pythonex4.py
@@ -6,8 +6,7 @@ try:
     fp = urllib.request.urlopen(x)
     mybytes = fp.read()
 
-    mystr = mybytes.decode("utf8")
-    find=0
+    mystr = mybytes.decode("utf8")   
     fp.close()
     print(mystr)
     print("the number of <p> is:"," ",mystr.count("</p>")," ","the number of <br> is "," ",mystr.count("<br>")," ","the number of href is:"," ",mystr.count("href"))# Ο τρόπος που βρίσκω τις αλλαγές γραμμής μέσω της ετικέτας <br> <p> και τους συνδέσμους
