354d3c73c87b72a73c739aacfbbf3ba17a381a45
fanismit <47718787+fanismit@users.noreply.github.com>
2019-02-17 18:06:36 +0200

    Add files via upload

---
+++ Pythonex4.py
@@ -0,0 +1,20 @@
+import urllib.request
+print("please insert a webpage") 
+x=input()
+x="https://"+x
+try:
+    fp = urllib.request.urlopen(x)
+    mybytes = fp.read()
+
+    mystr = mybytes.decode("utf8")
+    find=0
+    fp.close()
+    print(mystr)
+    print("the number of <p> is:"," ",mystr.count("</p>")," ","the number of <br> is "," ",mystr.count("<br>")," ","the number of href is:"," ",mystr.count("href"))# Ο τρόπος που βρίσκω τις αλλαγές γραμμής μέσω της ετικέτας <br> <p> και τους συνδέσμους
+except:
+    print("the url is not valid")
+
+
+
+
+
